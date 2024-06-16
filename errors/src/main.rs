249c98c8f4b431ac3404e7bcd61634ca660ba4f3
Artem Konovalov <tmknvlv@gmail.com>
2024-06-16 17:50:24 +0300

    Error handling & propagating

--- errors/src/main.rs
+++ errors/src/main.rs
@@ -1,5 +1,6 @@
+use std::fs;
 use std::fs::File;
-use std::io::{ErrorKind, Read};
+use std::io::{self, ErrorKind, Read};
 
 fn main() {
     let file_name = "src/main.rs";
@@ -18,7 +19,63 @@ fn main() {
         }
     };
 
+    /*
+    .unwrap() returns the value from Ok, or panic! on Err;
+    .expect() behaves the same but we can control an error message.
+     */
+
     let mut content = String::new();
     file.read_to_string(&mut content).unwrap();
     println!("{content}");
+}
+
+fn alternatives_to_match_with_results() {
+    let file = File::open("hello.txt").unwrap_or_else(|error| {
+        if error.kind() == ErrorKind::NotFound {
+            File::create("hello.txt").unwrap_or_else(|error| {
+                panic!("Problem creating the file: {error:?}");
+            })
+        } else {
+            panic!("Problem opening the file: {error:?}");
+        }
+    });
+}
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let username_file_result = File::open("hello.txt");
+
+    let mut username_file = match username_file_result {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut username = String::new();
+    match username_file.read_to_string(&mut username) {
+        Ok(_) => Ok(username),
+        Err(e) => Err(e),
+    }
+}
+
+fn read_username_from_file_short_version() -> Result<String, io::Error> {
+    let mut username_file = File::open("hello.txt")?;
+    let mut username = String::new();
+    username_file.read_to_string(&mut username)?;
+    Ok(username)
+}
+
+fn read_username_from_file_shortest() -> Result<String, io::Error> {
+    let mut username = String::new();
+    File::open("hello.txt")?.read_to_string(&mut username)?;
+    Ok(username)
+}
+
+fn read_username_from_file_orig() -> Result<String, io::Error> {
+    fs::read_to_string("hello.txt")
+}
+
+/**
+Operator ? can be used as with Result, as with Option for early return
+*/
+fn last_char_of_first_line(text: &str) -> Option<char> {
+    text.lines().next()?.chars().last()
 }
