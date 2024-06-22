0141c39a91dbb0fcfdb53d5e415ce8b37f378f4c
Artem Konovalov <tmknvlv@gmail.com>
2024-06-22 23:30:40 +0300

    Listing 10-11: A method that uses generic types different from its struct’s definition

--- generic_data_types/src/main.rs
+++ generic_data_types/src/main.rs
@@ -26,6 +26,20 @@ impl Point<f32> {
     }
 }
 
+struct Point2<X1, Y1> {
+    x: X1,
+    y: Y1,
+}
+
+impl<X1, Y1> Point2<X1, Y1> {
+    fn mixup<X2, Y2>(self, other: Point2<X2, Y2>) -> Point2<X1, Y2> {
+        Point2 {
+            x: self.x,
+            y: other.y,
+        }
+    }
+}
+
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
     let result = largest(&number_list);
@@ -39,5 +53,11 @@ fn main() {
     println!("p.x = {}", p.x());
 
     let fp: Point<f32> = Point { x: 3.2, y: 5.5 };
-    println!("Distance from origin {}", fp.distance_from_origin())
+    println!("Distance from origin {}", fp.distance_from_origin());
+
+    let p1 = Point2 { x: 5, y: 5.3 };
+    let p2 = Point2 { x: "Hello", y: 'c' };
+
+    let p3 = p1.mixup(p2);
+    println!("p3.x = {}, p3.y = {}", p3.x, p3.y)
 }
