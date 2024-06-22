52f467dbb7c2c0cde14fcfbe4c6a689521413133
Artem Konovalov <tmknvlv@gmail.com>
2024-06-22 22:31:55 +0300

    Listing 10-5: The largest function using generic type parameters; [this doesn’t yet compile]

--- generic_data_types/src/main.rs
+++ generic_data_types/src/main.rs
@@ -1,17 +1,5 @@
-fn largest_i32(list: &[i32]) -> &i32 {
+fn largest<T>(list: &[T]) -> &T {
     let mut largest = &list[0];
-    for item in list {
-        if item > largest {
-            largest = item;
-        }
-    }
-
-    largest
-}
-
-fn largest_char(list: &[char]) -> &char {
-    let mut largest = &list[0];
-
     for item in list {
         if item > largest {
             largest = item;
@@ -23,10 +11,10 @@ fn largest_char(list: &[char]) -> &char {
 
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
-    let result = largest_i32(&number_list);
+    let result = largest(&number_list);
     println!("The largest number is {result}");
 
     let char_list = vec!['a', 'b', 'c', 'd'];
-    let result = largest_char(&char_list);
+    let result = largest(&char_list);
     println!("The largest char is {result}");
 }
